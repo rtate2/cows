86c91f8a2960c40d743924ddde6d7ce1430d5d41
Randy Tate <randytatejr@gmail.com>
2019-10-28 19:14:26 -0500

    made getCows a promise

--- src/javascripts/components/cowList/cowList.js
+++ src/javascripts/components/cowList/cowList.js
@@ -5,13 +5,9 @@ import './cowList.scss';
 const buildCows = () => {
   cowData.getCows()
     .then((response) => {
-      const demCows = response.data.cows;
-      const cows = [];
-      Object.keys(demCows).forEach((cowId) => {
-        demCows[cowId].id = cowId;
-        cows.push(demCows[cowId]);
-      });
-      console.log('it worked!', cows);
+      console.log('cow array from cowList', response);
+
+      // call a domStringBuilder funtion and pass in a response (cows)
     })
     .catch((error) => {
       console.log('crap brok', error);
